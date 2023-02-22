d8211aed525afe71e7b23c05bd5d7eb34b64f9ad
Str3ltsov <andtaress@live.com>
2023-02-22 18:22:46 +0200

    Update es projektai page text

--- app/Http/Controllers/EsProjektaiController.php
+++ app/Http/Controllers/EsProjektaiController.php
@@ -8,6 +8,7 @@ use App\Traits\PageTextServices;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class EsProjektaiController extends Controller
@@ -41,5 +42,23 @@ class EsProjektaiController extends Controller
             ->with([
                 'pageText' => $this->decodePageText($this->pageText)
             ]);
+    }
+
+    /*
+ * Updates and saves es projektai page texts
+ */
+    public function update(Request $request): RedirectResponse
+    {
+        try {
+            $this->pageText->html_text = $request->html_text;
+            $this->pageText->save();
+
+            return redirect()
+                ->route('esProjektai')
+                ->with('success', __('Sėkmingai išsaugota ir atnaujinta'));
+
+        } catch (\Exception $exception) {
+            return back()->with('error', $exception->getMessage());
+        }
     }
 }
