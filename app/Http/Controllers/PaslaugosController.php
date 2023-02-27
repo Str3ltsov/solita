5506ccfcfb9dbc6daf5619527098f93276e26c85
Str3ltsov <andtaress@live.com>
2023-02-27 11:22:56 +0200

    Update paslaugos page text

--- app/Http/Controllers/PaslaugosController.php
+++ app/Http/Controllers/PaslaugosController.php
@@ -8,6 +8,7 @@ use App\Traits\PageTextServices;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class PaslaugosController extends Controller
@@ -41,5 +42,23 @@ class PaslaugosController extends Controller
             ->with([
                 'pageText' => $this->decodePageText($this->pageText)
             ]);
+    }
+
+    /*
+     * Updates and saves paslaugos page texts
+     */
+    public function update(Request $request): RedirectResponse
+    {
+        try {
+            $this->pageText->html_text = $request->html_text;
+            $this->pageText->save();
+
+            return redirect()
+                ->route('paslaugos')
+                ->with('success', __('Sėkmingai išsaugota ir atnaujinta'));
+
+        } catch (\Exception $exception) {
+            return back()->with('error', $exception->getMessage());
+        }
     }
 }
--- resources/views/paslaugos/index.blade.php
+++ resources/views/paslaugos/index.blade.php
@@ -7,16 +7,14 @@
     <main class="container d-flex flex-column justify-content-center gap-3" style="min-height: 70vh">
         @if (auth()->check())
             <div class="d-flex">
-                <a href="{{ route('paslaugosEdit') }}" class="btn btn-primary px-4 py-3">
+                <a href="{{ route('editPaslaugos') }}" class="btn btn-primary px-4 py-3">
                     <i class="fa-solid fa-pen-to-square me-1"></i>
                     {{ __('Redaguoti Paslaugos') }}
                 </a>
             </div>
         @endif
         <div class="mb-5 text-muted" style="line-height: 31px">
-            @foreach($pageTexts as $pageText)
-                {!! $pageText->html_text !!}
-            @endforeach
+            {!! $pageText->html_text !!}
         </div>
     </main>
 @endsection
