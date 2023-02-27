75d7239db90f8e6f6d97660338bc5b1e23178341
Str3ltsov <andtaress@live.com>
2023-02-27 13:48:48 +0200

    Template implementation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,24 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Title -->
     <title>{{ config('app.name', 'solita') }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('build/assets/app-67dcdfd2.css') }}" rel="stylesheet">
-    <link href="{{ asset('bootstrap-5.2.3-dist/css/bootstrap.css') }}" rel="stylesheet">
+{{--    <link href="{{ asset('build/assets/app-67dcdfd2.css') }}" rel="stylesheet">--}}
+{{--    <link href="{{ asset('bootstrap-5.2.3-dist/css/bootstrap.css') }}" rel="stylesheet">--}}
     <link href="{{ asset('fontawesome-free-6.3.0-web/css/all.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/meanmenu.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/backtotop.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome-pro.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/spacing.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
     <style>
         .ck-editor__editable {
             min-height: 500px;
@@ -21,14 +35,30 @@
 <body style="min-height: 100vh; min-width: 100vw">
     <!-- Layout -->
     @include('layouts.header')
+    <!-- Main content -->
     @yield('content')
+    <!-- Footer -->
     @include('layouts.footer')
     <!-- Scripts -->
-    <script src="{{ asset('build/assets/app-20b9e4fb.js') }}"></script>
-    <script src="{{ asset('jquery-3.6.3/jquery-3.6.3.min.js') }}"></script>
-    <script src="{{ asset('bootstrap-5.2.3-dist/js/bootstrap.bundle.js') }}"></script>
+{{--    <script src="{{ asset('build/assets/app-20b9e4fb.js') }}"></script>--}}
+{{--    <script src="{{ asset('jquery-3.6.3/jquery-3.6.3.min.js') }}"></script>--}}
+{{--    <script src="{{ asset('bootstrap-5.2.3-dist/js/bootstrap.bundle.js') }}"></script>--}}
     <script src="{{ asset('fontawesome-free-6.3.0-web/js/all.js') }}"></script>
     <script src="{{ asset('ckeditor5-build-classic/ckeditor.js') }}"></script>
+    <script src="{{ asset('assets/js/vendor/jquery.js') }}"></script>
+    <script src="{{ asset('assets/js/vendor/waypoints.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap-bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/meanmenu.js') }}"></script>
+    <script src="{{ asset('assets/js/swiper-bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/magnific-popup.js') }}"></script>
+    <script src="{{ asset('assets/js/parallax.js') }}"></script>
+    <script src="{{ asset('assets/js/backtotop.js') }}"></script>
+    <script src="{{ asset('assets/js/counterup.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.js') }}"></script>
+    <script src="{{ asset('assets/js/isotope-pkgd.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded-pkgd.js') }}"></script>
+    <script src="{{ asset('assets/js/ajax-form.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
     <script>
         ClassicEditor
             .create(document.querySelector('#editor'), {
