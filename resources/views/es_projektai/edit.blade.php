2daf72bb017c17a87651804807e0e7f6385e9530
Str3ltsov <andtaress@live.com>
2023-02-28 11:59:05 +0200

    Design for es projektai

--- resources/views/es_projektai/edit.blade.php
+++ resources/views/es_projektai/edit.blade.php
@@ -1,10 +1,26 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container mt-5">
-        @include('messages')
-    </div>
-    <main class="container d-flex flex-column justify-content-center gap-3" style="min-height: 70vh">
-        @include('es_projektai.form')
+    <main>
+        <section class="about__area pt-120 pb-60">
+            <div class="container">
+                <div class="row align-items-start wow fadeInUp" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="about__left-one p-relative mb-60">
+                            <div class="about__left-thumb">
+                                <img src="{{ asset('images/es_projektai.png') }}" alt="es_projektai">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-8">
+                        <div class="about__right-content mb-60">
+                            <div class="about__description-text">
+                                @include('es_projektai.form')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
--- resources/views/es_projektai/form.blade.php
+++ resources/views/es_projektai/form.blade.php
@@ -1,15 +1,23 @@
-<form method="POST" action="{{ route('updateEsProjektai') }}" class="d-flex flex-column gap-4 mb-5">
+<form method="POST" action="{{ route('updateEsProjektai') }}" class="d-flex flex-column gap-4 mt-10 mb-10">
     @csrf
     @method('PATCH')
     <textarea class="form-control text-muted" name="html_text" id="editor">
         {!! $pageText->html_text !!}
     </textarea>
     <div class="d-flex gap-2">
-        <button type="submit" class="btn btn-primary px-4 py-3">
-            {{ __('Atnaujinti ir Išsaugoti') }}
-        </button>
-        <a href="{{ url()->previous() }}" class="btn btn-secondary px-4 py-3">
-            {{ __('Grįžti Atgal') }}
-        </a>
+        <div class="banner5-btn wow fadeInUp mt-20 d-flex gap-4" data-wow-delay=".9s" style="visibility: visible; animation-delay: 0.9s; animation-name: fadeInUp;">
+            <div class="about__btn">
+                <button type="submit" class="it__btn-3 px-4">
+                    {{ __('Atnaujinti ir Išsaugoti') }}
+                    <i class="fa-solid fa-floppy-disk ms-2"></i>
+                </button>
+            </div>
+            <div class="about__btn">
+                <a class="it__btn-3 px-4" href="{{ url()->previous() }}">
+                    {{ __('Grįžti Atgal') }}
+                    <i class="fa-solid fa-chevron-left ms-2"></i>
+                </a>
+            </div>
+        </div>
     </div>
 </form>
